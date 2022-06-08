ce6d5852bda2ab6394510b7b8697ba968a6e80ad
qfeuilla <quentinfeuillademontixi42@gmail.com>
2022-06-09 00:28:43 +0200

    fix: fix the hyperarams class

--- stable_baseline_agent/agent/DQN_Agent.py
+++ stable_baseline_agent/agent/DQN_Agent.py
@@ -4,6 +4,7 @@ from bomberman.agents.BaseAgent		import BaseAgent
 from bomberman.states.State			import State, StatePlayer
 from bomberman.defines				import t_action
 from bomberman						import defines
+from dataclasses import dataclass
 
 from random import Random
 
@@ -19,74 +20,51 @@ from typing import Tuple
 tiles_watched = {k: i for i, k in enumerate(["B", "E", "W", "C", "r", "b", "s"," "])}
 
 
-class Hyperparams():
-	def __init__(
-		self,
-		lr : float = 1e-3,
-		gamma : float = 0.99,
-		optimizer : str = "optim.Adam",
-		replay_buffer_size : int = 10000,
-		target_update_regularity : int = 100,
-		epsilon_decay : float = 1e-3,
-		max_epsilon : float = 0.8,
-        min_epsilon : float = 0.001,
-		# batch_size : int # might want to add this
-		reset_concurent_agent_each : int = 10
-		training_games_amount : int = 10
-		max_game_duration : int = 50
-		skip_frames : int = 4 # not clear what this is to me
+@dataclass
+class Hyperparams:
+	lr : float = 1e-3
+	gamma : float = 0.99
+	# @Simon if you want to use string for optimizer and activation function, you need to do if else, you can't instantiate torch function like you did
+	# optimizer : str = "optim.Adam"
 
-		# net
-		activation_function : str = "nn.LeakyReLu",
-		map_conv0_chanels : int = 32,
-		map_conv0_kernel_size : int = 5,
-		map_conv1_chanels : int = 64,
-		map_conv1_kernel_size : int = 3,
-		map_conv2_chanels : int = 64,
-		map_conv2_kernel_size : int = 3,
-		map_avg_pool_kernel_size : int = 3,
-		map_linear_chanels : int = 32,
-		p_linear0_chanels : int = 28,
-		p_linear1_chanels : int = 16,
-		act_linear0_chanels : int = 32,
-		act_linear1_chanels : int = 16,
+	replay_buffer_size : int = 10000
+	target_update_regularity : int = 100
+	epsilon_decay : float = 1e-3
+	max_epsilon : float = 0.8
+	min_epsilon : float = 0.001
+	# batch_size : int # might want to add this
+	reset_concurent_agent_each : int = 10
+	training_games_amount : int = 10
+	max_game_duration : int = 50
+	skip_frames : int = 4 # not clear what this is to me
 
-		# training techniques
-		# TODO : insert in the code @Quentin
-		double_dqn : bool = False,
-		dueling_learning : bool = False,
-		prioritized_experience_replay : bool = False,
+	# net
 
-	) :
-		self.player_amount = player_amount
-		self.lr = lr
-		self.gamma = gamma
-		self.optimizer = optimizer
-		self.replay_buffer_size = replay_buffer_size
-		self.target_update_regularity = target_update_regularity
-		self.epsilon_decay = epsilon_decay
-		self.max_epsilon = max_epsilon
-		self.min_epsilon = min_epsilon
-		self.reset_agent_each = reset_agent_each
-		self.training_games_amount = training_games_amount
-		self.max_game_duration = max_game_duration
-		self.skip_frames = skip_frames
-		self.activation_function = activation_function
-		self.map_conv0_chanels = map_conv0_chanels
-		self.map_conv0_kernel_size = map_conv0_kernel_size
-		self.map_conv1_chanels = map_conv1_chanels
-		self.map_conv1_kernel_size = map_conv1_kernel_size
-		self.map_conv2_chanels = map_conv2_chanels
-		self.map_conv2_kernel_size = map_conv2_kernel_size
-		self.map_avg_pool_kernel_size = map_avg_pool_kernel_size
-		self.map_linear_chanels = map_linear_chanels
-		self.p_linear0_chanels = p_linear0_chanels
-		self.p_linear1_chanels = p_linear1_chanels
-		self.act_linear0_chanels = act_linear0_chanels
-		self.act_linear1_chanels = act_linear1_chanels
-		self.double_dqn = double_dqn
-		self.dueling_learning = dueling_learning
-		self.prioritized_experience_replay = prioritized_experience_replay
+	# activation_function : str = "nn.LeakyReLU" 
+	map_conv0_chanels : int = 32
+	map_conv0_kernel_size : int = 5
+	map_conv1_chanels : int = 64
+	map_conv1_kernel_size : int = 3
+	map_conv2_chanels : int = 64
+	map_conv2_kernel_size : int = 3
+	map_avg_pool_kernel_size : int = 3
+	map_linear_chanels : int = 32
+	p_linear0_chanels : int = 28
+	p_linear1_chanels : int = 16
+	act_linear0_chanels : int = 32
+	act_linear1_chanels : int = 16
+
+	# training techniques
+	# TODO : insert in the code @Quentin
+	double_dqn : bool = False
+	dueling_learning : bool = False
+	prioritized_experience_replay : bool = False
+
+	# reward shaping
+	# win_reward : float
+	# lose_reward : float
+	# box_broken_reward : float
+	# ...
 
 # TODO : log in mlflow @Simon & @Manu
 # something like inspect(class) to get all class params
@@ -102,26 +80,25 @@ class NeuralNetwork(nn.Module):
 		'''
 		super().__init__()
 
-		activation_function = exec(hp.activation_function + "()")
 
 		self.map_net = nn.Sequential(
 			nn.Conv2d(tile_types, hp.map_conv0_chanels, kernel_size=hp.map_conv0_kernel_size),
-			activation_function,
-			nn.Conv2d(hp.map_conv0_chanels, hp.map_conv1_chanels, kernel_size=hp.map_conv0_kernel_size),
-			activation_function,
-			nn.Conv2d(hp.map_conv1_chanels, hp.map_conv2_kernel_size, kernel_size=hp.map_conv2_chanels),
-			activation_function,
+			nn.LeakyReLU(),
+			nn.Conv2d(hp.map_conv0_chanels, hp.map_conv1_chanels, kernel_size=hp.map_conv1_kernel_size),
+			nn.LeakyReLU(),
+			nn.Conv2d(hp.map_conv1_chanels, hp.map_conv2_chanels, kernel_size=hp.map_conv2_kernel_size),
+			nn.LeakyReLU(),
 			nn.AvgPool2d(kernel_size=hp.map_avg_pool_kernel_size),
 			nn.Flatten(),
 			nn.Linear(hp.map_conv2_chanels, hp.map_linear_chanels),
-			activation_function,
+			nn.LeakyReLU(),
 		)
 
 		self.p_net = nn.Sequential(
 			nn.Linear(player_info + hp.map_linear_chanels, hp.p_linear0_chanels),
-			activation_function,
+			nn.LeakyReLU(),
 			nn.Linear(hp.p_linear0_chanels, hp.p_linear1_chanels),
-			activation_function
+			nn.LeakyReLU(),
 		)
 
 		# self.p2_net = nn.Sequential(
@@ -133,9 +110,9 @@ class NeuralNetwork(nn.Module):
 
 		self.action_taker = nn.Sequential(
 			nn.Linear(hp.map_linear_chanels + 2*hp.p_linear1_chanels, hp.act_linear0_chanels),
-			activation_function,
+			nn.LeakyReLU(),
 			nn.Linear(hp.act_linear0_chanels, hp.act_linear1_chanels),
-			activation_function,
+			nn.LeakyReLU(),
 			nn.Linear(hp.act_linear1_chanels, nb_action),
 			nn.Softmax()
 		)
@@ -161,7 +138,7 @@ class BuffedDQNAgent(BaseAgent):
 	'''
 
 	def __init__(self, player_num : int, hp : Hyperparams) -> None:
-		super().__init__(hp.player_num)
+		super().__init__(player_num)
 
 		self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -173,7 +150,7 @@ class BuffedDQNAgent(BaseAgent):
 		self.target_brain.load_state_dict(self.brain.state_dict())
 		self.target_brain.eval()
 
-		self.optimizer = exec(hp.optimizer + "(self.brain.parameters(), lr=hp.lr)")
+		self.optimizer = optim.Adam(self.brain.parameters(), lr=hp.lr)
 
 		self.hp = hp
 
@@ -227,10 +204,10 @@ class BuffedDQNAgent(BaseAgent):
 
 		curr_q_value = self.brain(state).gather(1, action)
 		next_q_value = self.target_brain(next_state).gather(  # Double DQN
-            1, self.brain(next_state).argmax(dim=1, keepdim=True)
-        ).detach()
+												1, self.brain(next_state).argmax(dim=1, keepdim=True)
+								).detach()
 		mask = 1 - done
-		target = (reward + self.gamma * next_q_value * mask).to(self.device)
+		target = (reward + self.hp.gamma * next_q_value * mask).to(self.device)
 
 		loss = F.smooth_l1_loss(curr_q_value, target)
 
